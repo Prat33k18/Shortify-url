45a05342f413b91aa5f41d2f0e41524b61b5f80d
Prateek <136463055+Prat33k18@users.noreply.github.com>
2025-05-08 13:44:00 +0530

    Update Config.jsx

--- src/API Config/Config.jsx
+++ src/API Config/Config.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const url = axios.create({
-  baseURL: 'http://localhost:8787', 
+  baseURL: process.env.REACT_APP_API_BASE_URL||'http://localhost:8787', 
 });
 
 export { url };
